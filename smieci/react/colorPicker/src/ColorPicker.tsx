0100f9890672e542d7c8cc2f7433b3795dbf3e75
Pola Walczynska <pola.walczynska@gmail.com>
2024-09-18 16:10:51 +0200

    add painting option

--- smieci/react/colorPicker/src/ColorPicker.tsx
+++ smieci/react/colorPicker/src/ColorPicker.tsx
@@ -9,11 +9,30 @@ function ColorPicker({ }: Props) {
         setColor(e.target.value)
     }
 
+    const handleDivClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const target = e.target as HTMLDivElement;
+        target.style.backgroundColor = color;
+    }
+
+    const createGrid = (size: number) => {
+        const grid = [];
+        for (let i = 0; i < size * size; i++) {
+            grid.push(
+                <div
+                    key={i}
+                    className='gridItem'
+                    onClick={handleDivClick}
+                ></div>
+            )
+        }
+        return grid;
+    }
+
     return (
         <div className='container'>
             <h1>Color Picker</h1>
-            <div className='display' style={{ backgroundColor: color }}>
-                <p>Selected Color: {color}</p>
+            <div className='gridContainer'>
+                {createGrid(20)}
             </div>
             <label>Select a color</label>
             <input type='color' value={color} onChange={handleColorChange} />
