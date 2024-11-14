86c15991e2435ed6417f6dc64cc4e459b232e202
Pola Walczynska <pola.walczynska@gmail.com>
2024-11-14 16:12:20 +0100

    use slf4j for logging

--- smieci/react/colorPicker/security/src/main/java/com/colorpicker/security/auth/AuthenticationService.java
+++ smieci/react/colorPicker/security/src/main/java/com/colorpicker/security/auth/AuthenticationService.java
@@ -5,6 +5,7 @@ import com.colorpicker.security.user.Role;
 import com.colorpicker.security.user.User;
 import com.colorpicker.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
@@ -45,7 +47,7 @@ public class AuthenticationService {
         var user = repository.findByEmail(request.email())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        System.out.println("user: " + user.getEmail() + " logged in");
+        log.info("Saving user: {}", user.getEmail());
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
